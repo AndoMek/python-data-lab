15563b3abe56e13f590a84bb8e19a9e29d4252ab
Andrey Anshin <a.anshin@godeltech.com>
2021-01-18 13:12:56 +0300

    Lesson-3. Functions. Class Properties. Class Private Attribute

---
+++ lesson_3/tasks/task_1.py
@@ -0,0 +1,47 @@
+""" Task 1: Create extended version of itertools.count() object
+
+Requirement:
+    1. Implement class Count based on Iterator
+
+    2. Class arguments:
+      a. ``name``: Name of count object. Optional. Default to None. String
+      b: Start ``count`` value. Optional. Default to 0. Integer or float
+      c: Count ``step``. Optional. Default to 1.
+
+    3. Mandatory methods:
+      a. implement __next__ method which return next counter value
+
+    4. Create read-only properties
+      a. name: counter name
+      a. curval: counter current value
+      b. nextval: counter next value (also it run __next__() method)
+
+    5. Use Type Hinting + Docstring
+"""
+
+
+from collections import Iterator
+
+
+class Count(Iterator):
+    """ Extend itertools.count() object """
+
+    def __next__(self):
+        pass
+
+
+if __name__ == "__main__":
+    print(help(Count))
+    print()
+
+    counter1 = Count(start=0, step=1)
+    print("Counter: %r" % counter1.name)
+
+    for _ in range(10):
+        print(next(counter1))
+
+    counter2 = Count(name="Awesome counter", start=2.5, step=0.5)
+    print("Counter: %r" % counter2.name)
+
+    for _ in range(10):
+        print(next(counter2))
